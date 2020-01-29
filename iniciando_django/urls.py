eea19e1f2217c1be5435b03675546bcd236d7074
Lucas Dev <lucas_ss.arts@live.com>
2020-01-28 21:23:08 -0300

    Rotas, controllers e views

--- iniciando_django/urls.py
+++ iniciando_django/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
+from my_app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^posts/$', views.posts_index),
+    url(r'^posts/(?P<post_id>\w)/$', views.posts_show),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -2,5 +2,24 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+def home(request):
+  return HttpResponse('Olá Mundo')
+
+def home_param(request, post_id):
+  return HttpResponse('Olá Mundo!!! %s' % post_id)
+
+def posts_index(request):
+  posts = Post.objects.all()
+  return render(request, 'posts/index.html', {
+    'posts': posts
+  })
+
+def posts_show(request, post_id):
+  post = Post.objects.get(id=post_id)
+  return render(request, 'posts/show.html', {
+    'post': post
+  })
